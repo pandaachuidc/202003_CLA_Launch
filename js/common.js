ac832e8a41ba86c1cbc67dfd54c45b82e5e4ef11
pandaachuidc <53323186+pandaachuidc@users.noreply.github.com>
2020-02-13 19:13:12 +0800

    improve RWD

--- js/common.js
+++ js/common.js
@@ -10,35 +10,49 @@ $(window).scroll(function() {
   });
 });
 
+//product details
+
 $(function() {
   $(".items ").click(function() {
     var $this = $(this);
-    $this
-      .addClass("active")
-      .siblings()
-      .removeClass("active");
-
+    if ($(this).hasClass("active")) {
+      $(this).removeClass("active");
+    } else {
+      $(".items").removeClass("active");
+      $this.addClass("active");
+    }
     return false;
   });
 });
 
 
-   $(".formfield.answer7").prepend('<span class="checkbox_span"></span>'),
-     $(".formfield.name input").attr("placeholder", "請輸入姓名"),
-     $(".formfield.mobile input").attr("placeholder", "請輸入手機號碼"),
-     $(".formfield.formfield_email input").attr(
-       "placeholder",
-       "請輸入電子信箱"
-     ),
-     $(".formfield.formfield_address1 input").attr("placeholder", "請輸入地址"),
-     //  $("#dwfrm_oto_oevent_answer1").attr("placeholder", "請輸入年齡"),
-     $("#dwfrm_oto_oevent_answer4").attr(
-       "placeholder",
-       "請輸入目前使用水狀精華,化妝水品牌/品名"
-     ),
-     $(".formfield.answer7 [for=dwfrm_oto_oevent_answer7]").html(
-       '提醒您送出資料後，即代表您同意將此資料提供予Lancôme 蘭蔻進行行銷處理與使用<a href="http://www.lancome.com.tw/customer-service-security-policy.html" target="_blank">[詳細個人資料權益說明]</a>'
-     ),
-     $(".formfield.formfield_button #otoo-event-submit-button span").text(
-       "立即體驗"
-     );
+
+$(".CLA-btn-try-btn").click(function() {
+  var _go = $(this).attr("href");
+  $("html,body").animate(
+    {
+      scrollTop: $(_go).offset().top 
+    },
+    2000
+  );
+});
+
+
+//表單
+
+$(".formfield.answer7").prepend('<span class="checkbox_span"></span>'),
+  $(".formfield.name input").attr("placeholder", "請輸入姓名"),
+  $(".formfield.mobile input").attr("placeholder", "請輸入手機號碼"),
+  $(".formfield.formfield_email input").attr("placeholder", "請輸入電子信箱"),
+  $(".formfield.formfield_address1 input").attr("placeholder", "請輸入地址"),
+  //  $("#dwfrm_oto_oevent_answer1").attr("placeholder", "請輸入年齡"),
+  $("#dwfrm_oto_oevent_answer4").attr(
+    "placeholder",
+    "請輸入目前使用水狀精華,化妝水品牌/品名"
+  ),
+  $(".formfield.answer7 [for=dwfrm_oto_oevent_answer7]").html(
+    '提醒您送出資料後，即代表您同意將此資料提供予Lancôme 蘭蔻進行行銷處理與使用<a href="http://www.lancome.com.tw/customer-service-security-policy.html" target="_blank">[詳細個人資料權益說明]</a>'
+  ),
+  $(".formfield.formfield_button #otoo-event-submit-button span").text(
+    "立即體驗"
+  );
